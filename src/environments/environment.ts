34b3bf498544e1be2327e93b018d5b2d256a1436
Rodoljub Tanasic <rodoljub.tanasic@outlook.com>
2025-08-11 04:39:17 +0200

    kjhkj

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api'
-    // apiUrl: 'https://basket-backend.onrender.com/api'
+  // apiUrl: 'http://localhost:3000/api'
+    apiUrl: 'https://basket-backend.onrender.com/api'
 
 };
