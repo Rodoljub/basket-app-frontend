1bdbba6f6d0a9a2316c12b04ca990931e7d5833d
Rodoljub Tanasic <rodoljub.tanasic@outlook.com>
2025-08-11 15:18:34 +0200

    url route

--- src/app/features/routes/route.service.ts
+++ src/app/features/routes/route.service.ts
@@ -1,26 +1,29 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { RouteStore } from "./route-store.model";
+import { environment } from "../../../environments/environment";
 
 // route.service.ts
 @Injectable({ providedIn: 'root' })
 export class RouteService {
+  private baseUrl = `${environment.apiUrl}/routes` 
+
   constructor(private http: HttpClient) {}
 
   getAll() {
-    return this.http.get<any[]>('http://localhost:3000/api/routes');
+    return this.http.get<any[]>(this.baseUrl);
   }
 
   getById(id: number) {
-    return this.http.get<any>(`http://localhost:3000/api/routes/${id}`)
+    return this.http.get<any>(`${this.baseUrl}/${id}`)
   }
 
   create(name: string) {
-    return this.http.post<any>('http://localhost:3000/api/routes', { name });
+    return this.http.post<any>(this.baseUrl, { name });
   }
 
   updateRoute(routeId: number, name: string, driverId: number) {
-    return this.http.put<RouteStore>(`http://localhost:3000/api/routes/${routeId}`, { name, driverId });
+    return this.http.put<RouteStore>(`${this.baseUrl}/${routeId}`, { name, driverId });
 }
   
 }
